7e4b1c0d3419229c4cdb8c2ff74a073b19595657
hikaru-takemoto <h.take1320@gmail.com>
2020-04-03 20:17:25 +0900

    :construction: #11 Qiitaログイン コールバック処理

---
+++ src/pages/api/login/callback.ts
@@ -0,0 +1,55 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import Session from '../../../models/session';
+
+import { env } from '../../../config/index';
+import * as auth from '../../../services/auth';
+import * as qiita from '../../../services/qiita/api';
+import { getCookie } from '../../../utils/cookie';
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  // パラメータ取得
+  const { code, state } = req.query;
+  if (
+    !code ||
+    !(typeof code == 'string') ||
+    !state ||
+    !(typeof state == 'string')
+  ) {
+    throw new Error('parameter error');
+  }
+
+  // cookies無しの場合はエラー
+  // TODO: クッキーのキーを型指定したい
+  const cookieSession = getCookie(req, 'session');
+
+  // sessionJWTのデコード
+  const payload = auth.dencryptSessionJwt(cookieSession);
+
+  // sessionの取得
+  const session = await Session.findByPk(payload.id, { raw: true });
+  if (!session) {
+    throw new Error('session not exists');
+  }
+
+  // stateの検証
+  if (state !== session.state) {
+    console.log('state');
+    console.log([state, session.state]);
+    throw new Error('state verify error');
+  }
+
+  // access_tokenのリクエスト
+  const postAccessToken = qiita.postAccessTokenFactory();
+  const accessToken = await postAccessToken(
+    env.qiitaClientId,
+    env.qiitaClientSecret,
+    code,
+  );
+
+  // session.access_tokenの更新
+  // 認証結果を返す
+  res.end();
+};
